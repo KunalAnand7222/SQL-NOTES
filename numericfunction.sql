cb597d78667734ac0cd50d7cdc069f39cb6b4fbc
Kunal Anand <161216758+KunalAnand7222@users.noreply.github.com>
2025-07-21 08:08:17 +0530

    Add files via upload

---
+++ numericfunction.sql
@@ -0,0 +1,14 @@
+#abs(x): return absolute value (positive)
+select abs(-10);
+#round(x,d): round upto d decimal places
+select round(10.5648,1);
+#ceil: return upper values
+#floor: return lower values
+select ceil(10.2);  #11
+select floor(10.2);  #10
+#mod(x,y): return remainder
+select mod(10,4); #2
+select 10%4;
+#rand: returns a random number between 0 and 1
+select rand();
+
